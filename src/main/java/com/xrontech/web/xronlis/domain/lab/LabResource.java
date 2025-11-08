ea41200338b4b4957bb3cccd489cc452b023b252
Aathif <111194791+mhdaathif@users.noreply.github.com>
2025-11-08 10:35:55 +0530

    get single lab - for admin & lab users and get filtered labs - for admin part block

--- src/main/java/com/xrontech/web/xronlis/domain/lab/LabResource.java
+++ src/main/java/com/xrontech/web/xronlis/domain/lab/LabResource.java
@@ -50,30 +50,30 @@ public class LabResource {
     }
 
     // get single lab - for admin & lab users
-    @GetMapping("/get/{lab-id}")
-    public ResponseEntity<Lab> getLabById(@PathVariable("lab-id") Long labId) {
-        return ResponseEntity.ok(labService.getLabById(labId));
-    }
+    // @GetMapping("/get/{lab-id}")
+    // public ResponseEntity<Lab> getLabById(@PathVariable("lab-id") Long labId) {
+    //     return ResponseEntity.ok(labService.getLabById(labId));
+    // }
 
     // get filtered labs - for admin
-    @GetMapping("/get/filter")
-    public ResponseEntity<List<Lab>> getFilteredLabs(
-            @RequestParam(required = false) String name,
-            @RequestParam(required = false) String branch,
-            @RequestParam(required = false) String fromDate,
-            @RequestParam(required = false) String toDate,
-            @RequestParam(required = false) Boolean status,
-            @RequestParam(required = false) Boolean delete,
-            @RequestParam(required = false) String mobile,
-            @RequestParam(required = false) String code,
-            @RequestParam(required = false, defaultValue = "createdAt") String sortBy,
-            @RequestParam(required = false, defaultValue = "asc") String sortDirection
-    ) {
-        List<Lab> labs = labService.getFilteredLabs(
-                name, branch,
-                fromDate, toDate,
-                status, delete, mobile, code, sortBy, sortDirection
-        );
-        return ResponseEntity.ok(labs);
-    }
+    // @GetMapping("/get/filter")
+    // public ResponseEntity<List<Lab>> getFilteredLabs(
+    //         @RequestParam(required = false) String name,
+    //         @RequestParam(required = false) String branch,
+    //         @RequestParam(required = false) String fromDate,
+    //         @RequestParam(required = false) String toDate,
+    //         @RequestParam(required = false) Boolean status,
+    //         @RequestParam(required = false) Boolean delete,
+    //         @RequestParam(required = false) String mobile,
+    //         @RequestParam(required = false) String code,
+    //         @RequestParam(required = false, defaultValue = "createdAt") String sortBy,
+    //         @RequestParam(required = false, defaultValue = "asc") String sortDirection
+    // ) {
+    //     List<Lab> labs = labService.getFilteredLabs(
+    //             name, branch,
+    //             fromDate, toDate,
+    //             status, delete, mobile, code, sortBy, sortDirection
+    //     );
+    //     return ResponseEntity.ok(labs);
+    // }
 }
